83b005fcd5bac61c6a2c4ff1f8659d215f5e853c
Manuel <manuel.devito@icloud.com>
2020-10-20 17:02:04 +0200

    Prima parte esercizio

---
+++ main.js
@@ -0,0 +1,24 @@
+// Scrivere un programma che stampi tutti i numeri da 1 a 100 con queste regole:
+// al posto dei multipli di 3, deve stampare "Fizz"
+// al posto dei multipli di 5, deve stampare "Buzz"
+// al posto dei multipli sia di 3 che di 5, deve stampare "FizzBuzz"
+
+
+
+for (var i = 1; i <= 100; i++) {
+    console.log(i);
+// Se è multiplo di 3 stampo Fizz.
+    if( i % 3 == 0 ){
+            console.log('Fizz');
+        }
+        // Se è multiplo di 5 stampo Buzz.
+        if( i % 5 == 0 ){
+            console.log('Buzz');
+        }
+        // Se è multiplo di 3 e 5 stampo FizzBuzz.
+        if () {
+
+        }
+
+
+}
